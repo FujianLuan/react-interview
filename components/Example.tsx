005e2962c555cbaa0c734fdc3bb8ea3023b8f9e5
luanfujian <215221938@qq.com>
2024-01-11 14:20:28 +0800

    feat: 接口对接

--- components/Example.tsx
+++ components/Example.tsx
@@ -12,6 +12,7 @@ type Repository = {
   forks_count: number
   url: string
   language: string
+  clone_url: string
   owner: {
     login: string
     avatar_url: string
@@ -34,6 +35,22 @@ export default function Example() {
   const [rawQuery, setRawQuery] = React.useState('')
   const query = rawQuery.toLowerCase().replace(/^[#>]/, '')
 
+  const [repos, setRepos] = React.useState<Repository[]>([])
+  const getQueryResult = async () => {
+    try {
+      const res = await fetch(`/api/search?q=${query}`)
+      const result: APIResponse = await res.json()
+      setRepos(result.items)
+      console.log('res', result)
+    } catch (error) {
+      console.error(error)
+    }
+  }
+
+  React.useEffect(() => {
+    getQueryResult()
+  }, [query])
+
   return (
     <Transition.Root
       show={open}
@@ -51,10 +68,10 @@ export default function Example() {
           leaveFrom="opacity-100"
           leaveTo="opacity-0"
         >
-          <div className="fixed inset-0 bg-gray-900 bg-opacity-40 transition-opacity" />
+          <div className="fixed inset-0 transition-opacity bg-gray-900 bg-opacity-40" />
         </Transition.Child>
 
-        <div className="fixed inset-0 z-10 overflow-y-auto p-4 sm:p-6 md:p-20">
+        <div className="fixed inset-0 z-10 p-4 overflow-y-auto sm:p-6 md:p-20">
           <Transition.Child
             as={React.Fragment}
             enter="ease-out duration-300"
@@ -64,20 +81,21 @@ export default function Example() {
             leaveFrom="opacity-100 scale-100"
             leaveTo="opacity-0 scale-95"
           >
-            <Dialog.Panel className="mx-auto max-w-xl transform divide-y divide-gray-500 divide-opacity-20 overflow-hidden rounded-2xl shadow-slate-300/10 bg-slate-900/70 shadow-2xl ring-1 ring-sky-500 ring-opacity-5 backdrop-blur-xl backdrop-filter transition-all">
+            <Dialog.Panel className="max-w-xl mx-auto overflow-hidden transition-all transform divide-y divide-gray-500 shadow-2xl divide-opacity-20 rounded-2xl bg-slate-900/70 shadow-slate-300/10 ring-1 ring-sky-500 ring-opacity-5 backdrop-blur-xl backdrop-filter">
               <Combobox
                 value=""
                 onChange={(item) => {
                   console.info('You have selected', item)
+                  window.open(item)
                 }}
               >
                 <div className="relative">
                   <MagnifyingGlassIcon
-                    className="pointer-events-none absolute top-3.5 left-4 h-5 w-5 text-gray-500"
+                    className="pointer-events-none absolute left-4 top-3.5 h-5 w-5 text-gray-500"
                     aria-hidden="true"
                   />
                   <Combobox.Input
-                    className="h-12 w-full border-0 bg-transparent pl-11 pr-4 text-gray-100 placeholder-gray-500 focus:ring-0 sm:text-sm focus:outline-0"
+                    className="w-full h-12 pr-4 text-gray-100 placeholder-gray-500 bg-transparent border-0 pl-11 focus:outline-0 focus:ring-0 sm:text-sm"
                     placeholder="Search GitHub repos..."
                     onChange={(event) => setRawQuery(event.target.value)}
                   />
@@ -85,20 +103,23 @@ export default function Example() {
 
                 <Combobox.Options
                   static
-                  className="max-h-80 scroll-py-10 scroll-pb-2 space-y-4 overflow-y-auto p-4 pb-2"
+                  className="p-4 pb-2 space-y-4 overflow-y-auto max-h-80 scroll-py-10 scroll-pb-2"
                 >
                   <li>
                     <h2 className="text-xs font-semibold text-gray-200">
                       Repositories
                     </h2>
-                    <ul className="-mx-4 mt-2 text-sm text-gray-700 space-y-0.5">
+                    <ul className="-mx-4 mt-2 space-y-0.5 text-sm text-gray-700">
+                      {/* <RepositoryOption />
                       <RepositoryOption />
-                      <RepositoryOption />
-                      <RepositoryOption />
+                      <RepositoryOption /> */}
+                      {repos?.map((repo) => {
+                        return <RepositoryOption repo={repo} />
+                      })}
                     </ul>
                   </li>
                 </Combobox.Options>
-                <span className="flex flex-wrap items-center bg-slate-900/20 py-2.5 px-4 text-xs text-gray-400">
+                <span className="flex flex-wrap items-center bg-slate-900/20 px-4 py-2.5 text-xs text-gray-400">
                   <FaceSmileIcon className="w-4 h-4 mr-1" />
                   Welcome to Zolplay&apos;s React Interview Challenge.
                 </span>
